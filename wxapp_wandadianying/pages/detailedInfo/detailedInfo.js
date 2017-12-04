9aaf5e3c4c3c493211a0ac1e6c9807ba0cc009d0
nzhingg <2258190228@qq.com>
2017-12-04 23:59:27 +0800

    :sparkles: 完成了电影详情页的数据同步

--- wxapp_wandadianying/pages/detailedInfo/detailedInfo.js
+++ wxapp_wandadianying/pages/detailedInfo/detailedInfo.js
@@ -2,15 +2,25 @@
 const app = getApp()
 Page({
   data: {
-    
+    item: [],
+    stills: []
   },
   onLoad: function(params) {
-    console.log(params);
+    // console.log(params);
+    var that = this;
     wx.request({
-      url:'https://www.easy-mock.com/mock/5a20be8ebe1c8248fef10573/getMoviesInfo/getInfo?id='+params,
+      url:'https://www.easy-mock.com/mock/5a20be8ebe1c8248fef10573/getMoviesInfo/getInfo',
       data:{},
       success: function(res){
-        console.log(res.data);
+        console.log(res.data.data.movieDetails);
+        var datas = res.data.data.movieDetails.filter((item)=>{
+          return item.id == params.id;
+        })
+        // console.log(datas[0].stills[]);
+        that.setData({
+          item:datas[0],
+          stills:datas[0].stills
+        });
       }
     })
   }
