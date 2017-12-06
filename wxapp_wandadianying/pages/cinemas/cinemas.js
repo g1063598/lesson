2ab828a975e06a854cb5da62bf7248771a24b2d5
nzhingg <2258190228@qq.com>
2017-12-06 19:54:59 +0800

    :sparkles: 完成了地址本地存储功能

--- wxapp_wandadianying/pages/cinemas/cinemas.js
+++ wxapp_wandadianying/pages/cinemas/cinemas.js
@@ -2,7 +2,20 @@ const app = getApp()
 
 Page({
     data: {
-        cinemas: []
+        cinemas: [],
+        cinema: ''
+    },
+    chooseCinema:function(e){
+        console.log(e.currentTarget.dataset.current);
+        var index = e.currentTarget.dataset.current;
+        console.log(this.data.cinemas[index]);
+        wx.setStorage({
+            key: "cinema",
+            data:this.data.cinemas[index].cinema
+        });
+        console.log(this.data.cinema)
+        wx.navigateBack({
+        });
     },
     onLoad: function() {
         var that = this;
@@ -10,8 +23,9 @@ Page({
             url: 'https://www.easy-mock.com/mock/5a20be8ebe1c8248fef10573/getMoviesInfo/getInfo',
             success: function(res) {
                 that.setData({
-                    cinemas: res.data.data.cinemas
-                })
+                    cinemas: res.data.data.cinemas,
+                });
+                // console.log(res.data.data.cinemas[0].cinema)
             }
         })
     }
