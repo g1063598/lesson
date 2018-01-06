d9ffea4cf7c80748acb1f7139fcb59abcc9357ee
nzhingg <2258190228@qq.com>
2018-01-06 21:19:41 +0800

    vue-todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+        <div id="app" class="container">
+            <div class="todo-wrapper">
+                <todo-header></todo-header>
+                <div class="todo-content">
+                    <todo-report></todo-report>
+                    <todo-list :collection="listState.items"></todo-list>
+                </div>
+                </div>    
+        </div>  
+    </template>
+    <style lang='stylus'>
+        @import '../styl/style.styl';
+    </style>
+        <script>
+            import ListStore from '../data/store.js';
+            import TodoHeader from './components/TodoHeader.vue';
+            import TodoReport from './components/TodoReport.vue';
+            import TodoList from './components/TodoList.vue';
+        
+            export default {
+                data () {
+                    return {
+                        listState: ListStore.state
+                    }
+                },
+                components: {
+                    TodoHeader,
+                    TodoReport,
+                    TodoList
+                }
+            }
+        </script>
+    
+    
+    
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="todo-list">
+        <todo-item v-for="(item,key) in collection" v-bind:key="key" :model="item"></todo-item>
+    </ul>
+</template>
+<script>
+    import TodoItem from './TodoItem.vue'
+    export default {
+    props: ['collection'],
+        components: {
+            TodoItem
+        } 
+    }
+</script>
+<style lang="stylus">
+    .todo-list 
+        list-style none    
+</style>
+       
+       
