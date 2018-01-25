c50a65d9aba561a0fa121cd9b1561ff10208aa38
nzhingg <2258190228@qq.com>
2018-01-25 21:01:51 +0800

    my-project

--- VUE/my-project/src/App.vue
+++ VUE/my-project/src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <div id="app">
-    <app-header v-on:titleChanged="updateTitle($event)" v-bind:title="title"></app-header>
-    <users v-bind:users="users"></users>
-    <users v-bind:users="users"></users>
-    <app-footer v-bind:title="title"></app-footer>
+    <ul>
+      <li><router-link to="/">home</router-link></li>
+      <li><router-link to="/HelloWorld">hello</router-link></li>      
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Users from './components/Users'
-import Footer from './components/Footer'
-import Header from './components/Header'
 
 export default {
   name: 'App',
-  components: {
-    "users": Users,
-    "app-header": Header,
-    "app-footer": Footer
-  },
   data() {
     return {
-      users:[
-        {name:"nnn",position:"web开发",show:false},
-        {name:"zzz",position:"web开发",show:false},
-        {name:"hhh",position:"web开发",show:false},
-        {name:"iii",position:"web开发",show:false},
-        {name:"nnn",position:"web开发",show:false},
-        {name:"ggg",position:"web开发",show:false},
-        {name:"nnn",position:"web开发",show:false}
-      ],
-      title:"传递的是一个值,(number string boolean)"
-    }
-  },
-  methods: {
-    updateTitle(title){
-      this.title=title;
     }
   }
 }
--- VUE/my-project/src/main.js
+++ VUE/my-project/src/main.js
@@ -1,10 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
+import HelloWorld from './components/HelloWorld'
+import Home from './components/Home'
 // import Users from './components/Users'
 
+
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+// 配置路由
+const router = new VueRouter({
+  routes:[
+    {path:"/",component:Home},
+    {path:"/helloworld",component:HelloWorld}
+  ],
+  mode: "history"
+})
 
 // 全局注册组件
 // Vue.component("users",Users);
@@ -12,6 +28,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
