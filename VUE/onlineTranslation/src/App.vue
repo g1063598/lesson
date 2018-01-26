fa8700c549bb45dd0243fce9cdfe848580a8c6b3
nzhingg <2258190228@qq.com>
2018-01-26 13:18:34 +0800

    A demo of online translation

---
+++ VUE/onlineTranslation/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>在线翻译</h1>
+    <h5>简单 / 易用 / 便捷</h5>
+    <translateForm v-on:formSubmit="translateText"></translateForm>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/translateForm'
+
+export default {
+  name: 'App',
+  methods: {
+    translateText(text){
+      alert(text);
+    }
+  },
+  components: {
+    TranslateForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VUE/onlineTranslation/src/components/translateForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="translateForm">
+      <form v-on:submit="formSubmit">
+        <input type="text" v-model="translateForm" placeholder="输入需要翻译的内容">
+        <select>
+            <option value="en">English</option>
+        </select>
+        <input type="submit" value="翻译">
+      </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data:function() {
+    return {
+        textToTranslate:""
+    }
+  },
+  methods: {
+      formSubmit(e){
+        //   alert(this.translateForm);
+          this.$emit("formSubmit",this.translateForm)
+          e.preventDefault();
+      }
+  }
+}
+</script>
+
+<style>
+</style>
