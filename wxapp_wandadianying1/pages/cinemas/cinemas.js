561f03306af91669d39776c82d11b4e1fdc6dcd2
nzhingg <2258190228@qq.com>
2018-03-17 23:10:59 +0800

    万达电影

---
+++ wxapp_wandadianying1/pages/cinemas/cinemas.js
@@ -0,0 +1,31 @@
+const app = getApp()
+
+Page({
+    data: {
+        cinemas: [],
+        cinema: ''
+    },
+    chooseCinema:function(e){
+        var index = e.currentTarget.dataset.current;
+        // console.log(this.data.cinemas[index]);
+        wx.setStorage({
+            key: "cinema",
+            data:this.data.cinemas[index].cinema
+        });
+        console.log(this.data.cinema)
+        wx.navigateBack({
+        });
+    },
+    onLoad: function() {
+        var that = this;
+        wx.request({
+            url: 'https://www.easy-mock.com/mock/5a20be8ebe1c8248fef10573/getMoviesInfo/getInfo',
+            success: function(res) {
+                that.setData({
+                    cinemas: res.data.data.cinemas,
+                });
+                // console.log(res.data.data.cinemas[0].cinema)
+            }
+        })
+    }
+})
